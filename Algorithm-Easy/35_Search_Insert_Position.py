c75e2b26c5c6f2f92a7619f8a7f5aef1561b9cab
xiangsong <1638392300@qq.com>
2019-03-12 13:37:20 +0800

    Update 35_Search_Insert_Position.py
    optimize a little

--- Algorithm-Easy/35_Search_Insert_Position.py
+++ Algorithm-Easy/35_Search_Insert_Position.py
@@ -5,9 +5,9 @@ class Solution:
         :type target: int
         :rtype: int
         """
-        if target > nums[len(nums) - 1]:
+        if target > nums[-1]:
             return len(nums)
-
+        
         for i in range(len(nums)):
             if nums[i] >= target:
                 return i
